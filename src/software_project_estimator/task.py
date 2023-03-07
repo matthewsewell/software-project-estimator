07c58f8a0f80aea6d63fa127d8ab3ebcfa39a54a
matthew <matthew@ragingbits.com>
2023-03-06 18:26:37 -0600

    added task group class

--- src/software_project_estimator/task.py
+++ src/software_project_estimator/task.py
@@ -1,18 +1,52 @@
 """
-Provides a task data model. A task has a name, estimates and an optional
-task group identifier.
+Provides a task data model. A task has a name and various estimates
 """
 
 import math
+from typing import List
 from uuid import UUID, uuid4
 
 from pydantic import BaseModel, Field  # pylint: disable=no-name-in-module
 
 
+class TaskGroup(BaseModel):
+    """
+    A group of tasks. This acts as a container for tasks. A task can be in more
+    than one group but it will then be processed multiple times. This may be
+    what you want if, for example, you have a QA task that always has the same
+    estimates and you want to include it in multiple sections of work.
+    """
+
+    name: str = Field(description="The name of the task group")
+    id_: UUID = Field(
+        default_factory=uuid4,
+        description="The unique identifier for the task group",
+    )
+    tasks: List["Task"] = Field(
+        default=[],
+        description="The tasks in the group",
+    )
+
+    @property
+    def task_count(self) -> int:
+        """Return the number of tasks in the group."""
+        return len(self.tasks)
+
+    def add_task(self, task: "Task") -> bool:
+        """Add a task to the group. Return True if the task was added."""
+        if task in self.tasks:
+            return False
+        self.tasks.append(task)
+        return True
+
+
 class Task(BaseModel):
     """Data model for our task object"""
 
-    id_: UUID = Field(default_factory=uuid4)
+    id_: UUID = Field(
+        default_factory=uuid4,
+        description="The unique identifier for the task group",
+    )
     name: str
     optimistic: float  # Optimistic estimate in person days
     pessimistic: float  # Pessimistic estimate in person days
