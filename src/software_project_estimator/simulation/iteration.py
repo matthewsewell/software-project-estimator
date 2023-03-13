fa460625464047c9217b9b47a985afa0abea4262
matthew <matthew@ragingbits.com>
2023-03-13 00:10:32 -0500

    initial work for a monte carlo single iteration using a state driven design pattern

---
+++ src/software_project_estimator/simulation/iteration.py
@@ -0,0 +1,170 @@
+"""
+This provides a single iteration for the monte carlo simulation.
+"""
+
+import datetime
+import logging
+from abc import ABC, abstractmethod
+from enum import Enum
+from typing import Optional
+
+from pydantic import BaseModel  # pylint: disable=no-name-in-module
+
+from software_project_estimator import Project
+
+logger = logging.getLogger(__name__)
+
+
+class IterationResultStatus(str, Enum):
+    """The status of the iteration result."""
+
+    SUCCESS = "success"
+    FAILURE = "failure"
+
+
+class IterationResult(BaseModel):  # pylint: disable=too-few-public-methods
+    """The result of an iteration."""
+
+    status: IterationResultStatus
+    message: Optional[str] = None
+    attributes: Optional[dict] = None
+
+
+class Iteration:
+    """
+    A single iteration of the monte carlo simulation. It maintains a context as
+    self.context and a result as self.result. The result will be None until the
+    iteration is complete. To complete the iteration, call self.run().
+    """
+
+    def __init__(self, project: Project) -> None:
+        self.context = IterationContext()
+        self.context.project = project
+
+    @property
+    def result(self) -> Optional[IterationResult]:
+        """Return the result of the iteration."""
+        return self.context.result
+
+    async def run(self) -> None:
+        """Run the iteration."""
+        self.context.transition_to(IterationStateUninitialized())
+        while self.context.result is None:
+            await self.context.process()
+
+
+class IterationContext:
+    """
+    The IterationContext defines the interface for running a full iteration.
+    """
+
+    project: Optional[Project] = None
+    result: Optional[IterationResult] = None
+
+    # This is the temporary result of the iteration. It is used to store what
+    # we think is the result of the iteration until we formatlize it by setting
+    # self.result.
+    provisional_result: Optional[IterationResult] = None
+
+    # This is the internal state of the context
+    _state: Optional["IterationBaseState"] = None
+
+    def transition_to(self, state: "IterationBaseState"):
+        """
+        The Context allows changing the State object at runtime.
+        """
+
+        self._state = state
+        self._state.context = self
+
+    async def process(self):
+        """
+        The Context delegates part of its behavior to the current State object.
+        """
+        await self._state.handle_process()
+
+
+class IterationBaseState(ABC):
+    """
+    The base State class declares methods that all Concrete State should
+    implement and also provides a backreference to the Context object,
+    associated with the State. This backreference can be used by States to
+    transition the Context to another State.
+    """
+
+    @property
+    def context(self) -> IterationContext:  # pylint: disable=missing-function-docstring
+        return self._context
+
+    @context.setter
+    def context(
+        self, context: IterationContext
+    ) -> None:  # pylint: disable=missing-function-docstring
+        self._context = context
+
+    @abstractmethod
+    async def handle_process(self) -> None:
+        """Just a gentle reminder to set this method on every state."""
+        raise NotImplementedError
+
+
+class IterationStateUninitialized(IterationBaseState):
+    """
+    The Uninitialized state is the initial state of the iteration.
+    """
+
+    async def handle_process(self) -> None:
+        """This is where we initialize the iteration."""
+        logger.debug("Iteration is initializing.")
+        if not self.context.project or not isinstance(self.context.project, Project):
+            self.context.provisional_result = IterationResult(
+                status=IterationResultStatus.FAILURE,
+                message="No project was provided.",
+            )
+            self.context.transition_to(IterationStateError())
+            return
+
+        if all(
+            [
+                not self.context.project.tasks,
+                not self.context.project.task_groups,
+            ]
+        ):
+            self.context.provisional_result = IterationResult(
+                status=IterationResultStatus.FAILURE,
+                message="No tasks were present in the project.",
+            )
+            self.context.transition_to(IterationStateError())
+            return
+
+        self.context.transition_to(IterationStateSuccessful())
+
+
+class IterationStateError(IterationBaseState):
+    """
+    The Error state is the state of the iteration when an error has occurred.
+    """
+
+    async def handle_process(self) -> None:
+        logger.debug("Iteration has encountered an error.")
+        self.context.result = self.context.provisional_result or IterationResult(
+            status=IterationResultStatus.FAILURE,
+            message="Iteration failed unexpectedly. Someone should fix this.",
+        )
+
+
+class IterationStateSuccessful(IterationBaseState):
+    """
+    The Successful state is the final state of the iteration.
+    """
+
+    async def handle_process(self) -> None:
+        logger.debug("Iteration is successful.")
+        end_date = (
+            datetime.date.today()
+        )  # STUB: This should be the end date of the iteration.
+        self.context.result = IterationResult(
+            status=IterationResultStatus.SUCCESS,
+            message="Iteration completed successfully.",
+            attributes={"end_date": end_date},
+        )
