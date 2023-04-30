d20a362291f6523cfdcaef70d7e6045fd9de9253
matthew <matthew@ragingbits.com>
2023-04-29 20:21:06 -0500

    moved file structure

---
+++ examples/load_from_csv.py
@@ -0,0 +1,15 @@
+"""
+Loads a CSV file of tasks into a project and prints out the results of a
+monte carlo simulation.
+"""
+import csv
+import os
+
+from software_project_estimator import Project, Task
+from software_project_estimator.simulation.monte_carlo import MonteCarlo
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
