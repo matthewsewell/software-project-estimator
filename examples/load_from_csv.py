c0eccbbd67c8dca987396f9398fb15cd93e8b7e9
matthew <matthew@ragingbits.com>
2023-04-30 15:08:17 -0500

    good enough docsumentation for now

--- examples/load_from_csv.py
+++ examples/load_from_csv.py
@@ -9,7 +9,29 @@ from software_project_estimator import Project, Task
 from software_project_estimator.simulation.monte_carlo import MonteCarlo
 
 def main():
-    print("Hello World!")
+    file_directory = os.path.dirname(os.path.realpath(__file__))
+    csv_file = os.path.join(file_directory, "assets", "fake-project.csv")
+    with open(csv_file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        tasks = []
+        for row in reader:
+            row['optimistic'] = float(row['optimistic'])
+            row['pessimistic'] = float(row['pessimistic'])
+            row['likely'] = float(row['likely'])
+            tasks.append(Task(**row))
+    project = Project(
+        name="This is an example project",
+        tasks=tasks,
+        developer_count=3,
+        work_hours_per_day=6.5,
+        communication_penalty=0.5,
+    )
+    monte = MonteCarlo(project, 100_000)
+    results = monte.run()
+
+    for day, result in results.items():
+        print(f"{day}: {result}")
+
 
 if __name__ == "__main__":
     main()
